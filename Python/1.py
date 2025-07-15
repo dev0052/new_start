39b6816373649e40ca65b32756aa5a2f4bb0a857
dev0052 <realdev0052@gmail.com>
2025-07-15 09:36:53 +0530

    added new line and tabbed line feature to print statement

--- Python/1.py
+++ Python/1.py
@@ -1,2 +1,3 @@
 print("Hello World!");
 print("This is a Python script.")
+print("We can also write multiple lines of code.\nThis is the second line and will be printed on a new line.\nWe can also use '\ t'for tab spaces.for example,\n\t\tHere is a tabbed line which is also a new line.")
